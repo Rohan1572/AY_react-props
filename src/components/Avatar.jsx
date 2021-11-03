8c36363468f992f3fc779af6e55a67c4807b0377
Rohan1572 <hellorohan02@gmail.com>
2021-11-03 17:16:55 +0530

    1716

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Avatar(props) {
+  return <img src={props.img} alt="avatar_img" className="circle-img" />;
+}
+
+export default Avatar;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import react from "react";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img src={props.img} alt="avatar_img" className="circle-img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
